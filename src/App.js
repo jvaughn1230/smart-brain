160d13c29c5d8c95fbe783aa11ea7568191a463a
Jeffrey Alex Vaughn <vaughnj29@gmail.com>
2022-03-24 07:19:32 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import Navigation from './components/navigation/Navigation';
+import Logo from './components/logo/Logo';
+import ImageLinkForm from './components/imageLinkForm/ImageLinkForm';
+import Rank from './components/rank/Rank';
+import Particles from "react-tsparticles";
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Navigation />
+      <Logo />
+      <Rank />
+      <ImageLinkForm />
+      
+      {/*
+        <FaceRecognition />
+      */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/logo/Logo.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Tilt from 'react-parallax-tilt';
+import brain from './brain.png'
+
+const Logo = () => {
+    return (
+        <div className='ma4 mt0'>
+            <Tilt className='Tilt br2 shadow-2' style={{width:'150px'}}> 
+                <div className='Tilt-inner pa3' style={{ height: '150px', width:'150px'}}>  
+                    <img style={{paddingTop: '5px'}}src={brain} alt='logo' />
+                </div>
+            </Tilt>
+        </div>
+    );
+}
+
+export default Logo;
