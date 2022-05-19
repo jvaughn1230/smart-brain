a95a2ad9d4d44696d38e6dbc6df029a7ea9fb457
Jeffrey Vaughn <jvaughn1230@gmail.com>
2022-05-19 14:32:29 -0700

    debugging

--- src/components/register/Register.jsx
+++ src/components/register/Register.jsx
@@ -23,7 +23,7 @@ class Register extends React.Component{
   }
 
   onSubmitSignIn = () => {
-    fetch('http://localhost:3000/register', {
+    fetch('https://serene-sierra-96576.herokuapp.com/register', {
       method: 'POST',
       headers:{'Content-Type': 'application/json'},
       body: JSON.stringify({
